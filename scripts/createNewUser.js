c6bce8565bf3d99aa3e515b072d1e1857c4a8097
Szilvia Kapuvári <kapuvari.szilvia@gmail.com>
2019-07-13 19:38:27 +0200

    validátorok (név, mail, address), új default 'none' input mezők új júzer hozzáadásához,NextID function

--- scripts/createNewUser.js
+++ scripts/createNewUser.js
@@ -1,56 +1,7 @@
-window.addEventListener('load', Window_LoadHandler, false);
-
-function window_LoadHandler() {
-  var fieldForMessages = document.getElementById('fieldForMessages');
-  fieldForMessages.addEventListener('click', clearFields, false);
-  var saveNewUserBtn = document.getElementById('saveNewUserBtn');
-  saveNewUserBtn.addEventListener('click', userNameValidation, false);
-  var addNewUserName = document.getElementById('addNewUserName');
-  addNewUserName.addEventListener();
-  var addNewUserEmail = document.getElementById('addNewUserEmail');
-  addNewUserEmail.addEventListener();
-  var addNewUserAddress = document.getElementById('addNewUserAddress');
-  addNewUserAddress.addEventListener();
-
-  //   addEventListeners - saveNewUserData, focusevents, errors;
-}
-
-function userNameValidation() {
-  for (var i = 0; i < users.length; i += 1) {
-    if (addNewUserName === users[i].name) {
-      fieldForMessages.innerHTML = 'Username already exists.';
-      break;
-    } if (addNewUserName !== /^[a-zA-Z]/) {
-      fieldForMessages.innerHTML = 'Please, write letters only!';
-    } else {
-      saveNewUserData();
-    }
+class UserManager {
+  constructor(data) {
+    this.data = data;
   }
+
 }
-
-function saveNewUserData() {
-  var arrayForSavedUserObj = [];
-  var saveNewUserDate = {};
-
-  saveNewUserData.name = document.getElementById('addNewUserName').value;
-  saveNewUserData.email = document.getElementById('addNewUserEmail').value;
-  saveNewUserData.address = document.getElementById('addNewUserAddress').value;
-
-  arrayForSavedUserObj.push(saveNewUserData);
-  fieldForMessages.innerHTML =
-
-
-
-  // üzi a sikeres mentésről
-  // táblázatgeneráló meghívása
-  clearFields();
-}
-
-function clearFields() {
-  fieldForMessages.innerHTML = '';
-}
-
-// function setTimeout() {
-//   return timeout;
-// }
 
